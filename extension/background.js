848ec62db921499f94026df2b0c6796926b1a045
ishanyachanana <71649633+ishanyachanana@users.noreply.github.com>
2026-05-11 18:08:07 +0530

    Update background.js

--- extension/background.js
+++ extension/background.js
@@ -1,5 +1,4 @@
 const API = "https://ingredient-scanner-mu.vercel.app/api/analyze-ingredients";
-const SECRET = "PC7lbIDqBOrh9UWpiWcpACLE3mQ1ofGjtgeb98fd";
 
 chrome.runtime.onMessage.addListener((msg, _sender, sendResponse) => {
   if (msg.type !== "analyze") return;
@@ -15,7 +14,7 @@ chrome.runtime.onMessage.addListener((msg, _sender, sendResponse) => {
 
       const r = await fetch(API, {
         method: "POST",
-        headers: { "Content-Type": "application/json", "X-Client-Secret": SECRET },
+        headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ name: msg.name, ingredients: msg.ingredients }),
         signal: controller.signal,
       });
